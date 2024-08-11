f1591955b78e4e50d410983ad2e59e8d2d4a051b
deepanAppycodes <deepan@appycodes.com>
2024-08-12 01:56:47 +0530

    url updated

--- config/db.config.js
+++ config/db.config.js
@@ -1,12 +1,14 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
-
-const databaseInstance = new Sequelize('all_sports', 'root','1234',{
-    host:'localhost',
-    dialect:"mysql"
-});
-
-
+const databaseInstance = new Sequelize(
+  process.env.DB_NAME,
+  process.env.USER_NAME,
+  process.env.USER_PASSWORD,
+  {
+    host: process.env.HOST_NAME,
+    dialect: "mysql",
+  }
+);
 
 module.exports = databaseInstance;
